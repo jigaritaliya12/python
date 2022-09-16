f2881173b15ed8a0a00e60d83ef62de185e0123f
jigaritaliya12 <95684476+jigaritaliya12@users.noreply.github.com>
2022-09-16 12:15:13 +0530

    new

---
+++ jigar.harry/01_pr.py
@@ -0,0 +1,8 @@
+mydict ={
+    "pankha":"fan",
+    "dabba":"box",
+    "vastu":"item",
+}
+print("optin are ", mydict.keys())
+a=input("enter your hindi word \n")
+print("the meaning of your word is:",mydict[a])
