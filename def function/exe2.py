679ac12d022298d24e1cf47999987e2c1a0f1170
jigaritaliya12 <95684476+jigaritaliya12@users.noreply.github.com>
2022-09-26 15:06:34 +0530

    new

--- def function/exe2.py
+++ def function/exe2.py
@@ -1,7 +1,7 @@
 '''write a program that has function that return area of triangle from given base and height in which  height is optional. (use default arguments values)'''
 
 def getarea(base,height):
-    area =1/2 (base * height)
+    area =(base * height)/2
     return area
 
 a=int(input("enter your base"))
