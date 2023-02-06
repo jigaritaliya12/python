a1f7a3d7b716de2b3dc5f90f89f3a6324341f978
jigaritaliya12 <95684476+jigaritaliya12@users.noreply.github.com>
2023-02-06 15:15:29 +0530

    new

---
+++ rivision/enumerate.py
@@ -0,0 +1,17 @@
+#without enumerate
+
+# marks=[12,25,34,78,40,30,3,2]
+# index=0
+# for mark in marks:
+#     print(mark)
+#     if(index==3):
+#      print("jigar,awesom")
+#     index+=1
+    
+#with enumerate
+marks=[12,25,34,78,40,30,3,2]
+for index,mark in enumerate(marks):
+    print(mark)
+    if(index==3):
+        print("jigar,awasom")
+    index+=1
---
+++ rivision/exeption-handling.py
@@ -0,0 +1,3 @@
+a=int(input("enter your number"))
+for i in range(1,11):
+    print(a*i)
